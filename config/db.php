f08fc559fa044d2a211c66fbf318fea7621cfbeb
non0921 <kirin161@gmail.com>
2026-03-16 22:52:12 +0900

    dbconnect function

--- config/db.php
+++ config/db.php
@@ -1,3 +1,20 @@
 <?php
 
+$dsn = "mysql:host=localhost;dbname=bbs;charset=utf8mb4";
+$user = "root";
+$password = "root";
+
+try {
+    $pdo = new PDO($dsn, $user, $password);
+
+    $pdo->setAttribute(PDO::ATTR_ERROMODE, PDO::ERROMODE_EXCEPTION); //PDOExceptionにエラーをthrowしている(catchでエラーを受け取れるようにしている)
+
+} catch(PDOException $e) {
+
+    echo "DB接続失敗";
+    $e->getMessage();
+    exit;
+
+}
+
 ?>
