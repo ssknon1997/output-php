e701cae12328369549f06573351cc4ede6203dd6
non0921 <taruto7771@gmail.com>
2026-03-20 12:38:46 +0900

    更新処理の追加

---
+++ post/update.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+require_once('../config/db.php');
+
+$id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
+$content = filter_input(INPUT_POST, 'content');
+
+$sql = "UPDATE posts SET content=? WHERE id=? AND user=?"; //postsテーブルのcontent更新するするためにPOST送信されたidとuser_idが合うレコードを更新する
+
+$stmt = $pdo->prepare($sql);
+
+$stmt->execute([
+    $content,
+    $id,
+    $_SESSION['user']['id']
+]);
+
+header("Location: ../index.php");
