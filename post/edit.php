783615dfb8c9e7aac0d3b3fc1005efcac233bc10
non0921 <taruto7771@gmail.com>
2026-03-20 12:21:44 +0900

    編集機能の追加

---
+++ post/edit.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+require_once('../config/db.php');
+
+$id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+
+$sql = "SELECT * FROM posts WHERE id = ?";
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute([$id]);
+
+$post = $stmt->fetch(PDO::FETCH_ASSOC);
+
+?>
+
+<h2>投稿編集</h2>
+
+<form action="update.php" method="POST">
+
+    <input type="hidden" name="id" value="<?php echo htmlspecialchars($post['id']); ?>" >
+
+    <textarea name="content">
+        <?php echo htmlspecialchars($post['content']); ?>
+    </textarea>
+
+    <button>更新</button>
+
+</form>
