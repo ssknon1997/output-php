68cf50b592169cfe8815996213757627e5c2a286
non0921 <taruto7771@gmail.com>
2026-03-21 12:46:48 +0900

    トークンを検証するif文を追加

--- post/create.php
+++ post/create.php
@@ -1,8 +1,16 @@
 <?php
 
 session_start();
-
 require_once('../config/db.php');
+
+if(!isset($_POST['csrf_token']) || 
+    !hash_equals($_SESSION['csrf_token'], $_POST['csrf_token']
+    )) { //!issetで入ってなかったらtrueもしくはフォームに埋め込まれたトークンとサーバに保存されたトークンが合わなければtrue
+    exit('不正なリクエスト');
+}
+
+$_SESSION["csrf_token"] = bin2hex(random_bytes(32)); //トークンの使い回しを防ぐためにトークンを再生成
+
 
 if(!isset($_SESSION["user"])) { //sessionのuserが入ってない場合ログイン画面へ
 
