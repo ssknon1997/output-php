24b8220200f23cfc6e52bb4143382315609b2214
non0921 <taruto7771@gmail.com>
2026-03-25 19:54:24 +0900

    ログアウトページを追加

--- auth/logout.php
+++ auth/logout.php
@@ -1,6 +1,9 @@
 <?php
+require_once('../includes/function.php');
 // session_startを呼んでください
-
+session_start();
 // session_destroyでセッション情報を削除してください
-
+session_destroy();
 // auth/login.phpにリダイレクトしてexitしてください
+redirect('auth/login.php');
+exit;
